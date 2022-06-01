091e5d52481fb05daee8f7b3020eb53363b6eef2
tom <balditommaso22@gmail.com>
2022-06-01 09:07:07 +0200

    init spark

---
+++ spark/bloomfilter/bloomfilter.py
@@ -0,0 +1,12 @@
+import numpy as np
+from pyspark import SparkContext
+
+
+def array_split(line):
+    np.array(line.split("/t"))
+
+
+sc = SparkContext(appName="Bloomfilter", master="local[*]")
+
+rdd_file = sc.textFile("film-ratings.txt").map(array_split)
+print(rdd_file.take(4).collect())
