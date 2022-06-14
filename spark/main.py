2cc6afa4cc87d9a76a2ce0c14f2bf7d4390a2d83
mrfransis <franceh96@hotmail.it>
2022-06-14 21:46:14 +0100

    fix

--- spark/main.py
+++ spark/main.py
@@ -56,6 +56,11 @@ if __name__ == "__main__":
     sc.addPyFile("bloomfilter.zip")
     rdd_input = sc.textFile(INPUT_PATH).cache()
 
+    #Only for testing
+    #for p in [0.01, 0.05, 0.1, 0.2]:
+        #for iter in range(10):
+            #print("\n> P:", p, " iter: ",iter)
+
     # Parameter Calibration stage
     start_time = time.time()
 
@@ -63,8 +68,8 @@ if __name__ == "__main__":
     rdd_counts_by_rating = rdd_films.map(lambda x: (x[1], 1)).reduceByKey(lambda x, y: x + y).sortByKey()
     counts_by_rating = rdd_counts_by_rating.collect()
 
-    execution_time = time.time() - start_time
-    print("\n> Stage 0 execution time:", execution_time, "s")
+    execution_time_stage0 = time.time() - start_time
+    #print("\n> Stage 0 execution time:", execution_time_stage0, "s")
 
     # Bloom Filter Creation stage
     start_time_stage1 = time.time()
@@ -74,8 +79,8 @@ if __name__ == "__main__":
     rdd_partial_bf = rdd_input.mapPartitions(insert_in_bloomfilters)
     rdd_final_bf = rdd_partial_bf.reduceByKey(lambda filter1, filter2: filter1.bitwise_or(filter2)).sortByKey()
 
-    execution_time = time.time() - start_time_stage1
-    print("\n> Stage 1 execution time:", execution_time, "s")
+    execution_time_stage1 = time.time() - start_time_stage1
+    #print("\n> Stage 1 execution time:", execution_time_stage1, "s")
 
     # Parameter Validation Stage
     start_time_stage2 = time.time()
@@ -84,8 +89,8 @@ if __name__ == "__main__":
     rdd_counter = rdd_input.flatMap(validate_bloomfilter)
     rdd_false_positive_count = rdd_counter.reduceByKey(lambda x, y: x + y).sortByKey()
 
-    execution_time = time.time() - start_time_stage2
-    print("\n> Stage 2 execution time:", execution_time, "s")
+    execution_time_stage2 = time.time() - start_time_stage2
+    #print("\n> Stage 2 execution time:", execution_time_stage2, "s")
 
     false_positive_count = rdd_false_positive_count.collect()
     counts_by_rating = rdd_counts_by_rating.collect()
@@ -97,4 +102,5 @@ if __name__ == "__main__":
             f.write(str(i+1) + "\t" + str(false_positive_rate) + "\n")
 
     execution_time = time.time() - start_time
-    print("\n> Total execution time:", execution_time, "s")
+    #print("\n> Total execution time:", execution_time, "s")
+    #print(p,",",iter,",",execution_time_stage0,",",execution_time_stage1,",",execution_time_stage2)
